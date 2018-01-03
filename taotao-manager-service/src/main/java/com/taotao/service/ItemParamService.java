93585578687e88271eb285a8e8a933cb610b8875
yufeng <2360316295@qq.com>
2018-01-03 23:35:44 +0800

    商品类目规格参数的总体包含项实现

---
+++ taotao-manager-service/src/main/java/com/taotao/service/ItemParamService.java
@@ -0,0 +1,14 @@
+package com.taotao.service;
+
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.pojo.TbItemParam;
+
+/**
+ * 规格参数模板Service
+ * @author yufeng 2018/1/3
+ */
+public interface ItemParamService {
+
+	TaotaoResult getItemParamByCid(long cid);
+	TaotaoResult insertItemParam(TbItemParam itemParam);
+}
