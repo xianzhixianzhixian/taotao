72eb33e2135a00d61ebac002a35d9e07a66eb65d
FanYufeng02 <yufeng.fan02@ele.me>
2018-01-09 20:53:51 +0800

    更新xml文件约束

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    /**
+     * 打开首页
+     * @return
+     */
     @RequestMapping("/index")
     public String showIndex(){
         return "index";
